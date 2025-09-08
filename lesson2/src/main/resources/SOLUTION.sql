8b0a3bb8185bb9e9e4ad1cb367694a88c78ded57
sinsenti <sinsenti73@gmail.com>
2025-09-08 16:07:12 +0300

    Auto-push: 2025-09-08 16:07:12

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -29,12 +29,25 @@ VALUES
   ('Math2', 'math 2', 5),
   ('Math3', 'math 3', 5);
 
-INSERT INTO
-  paymenttype (name)
-VALUES
-  ('DAILY'),
-  ('WEEKLY'),
-  ('MONTHLY');
+insert into
+  paymenttype (id, name)
+values
+  (1, 'DAILY');
+
+insert into
+  paymenttype (id, name)
+values
+  (3, 'YEAR');
+
+insert into
+  paymenttype (id, name)
+values
+  (2, 'WEEKLY');
+
+insert into
+  paymenttype (id, name)
+values
+  (4, 'MONTHLY');
 
 INSERT INTO
   payment (type_id, amount, student_id, payment_date)
